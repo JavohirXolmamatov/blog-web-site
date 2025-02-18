c70cdb0ef2f0224c2197fcc3ead1e1da2c0c4ee5
JavohirXolmamatov <xolmamatovjavohir2002@gmail.com>
2025-02-18 10:49:53 +0500

    Full-stack loyiha start

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import { Login, Main, Register } from "./components";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
