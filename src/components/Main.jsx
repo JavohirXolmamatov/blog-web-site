0be85a32599e14e9f69d33cd133c00dea2df3c4d
JavohirXolmamatov <xolmamatovjavohir2002@gmail.com>
2025-03-01 11:06:15 +0500

    Loader

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import Loader from "../ui/Loader";
 
 function Main() {
-  const { articles } = useSelector((state) => state.article);
-  // console.log(articles);
+  const { articles, isLoading } = useSelector((state) => state.article);
 
   return (
     <div className="album py-5">
-      <div className="container">
+      {isLoading ? <Loader /> : <></>}
+
+      <div className="container p-0">
         <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
           {articles &&
             articles.map((item) => (
@@ -28,33 +29,33 @@ function Main() {
                     <rect width="100%" height="100%" fill="#55595c"></rect>
                   </svg>
                   <div className="card-body">
-                    <p className="card-text fw-bold">{item.title}</p>
+                    <p className="card-text fw-bold m-0">{item.title}</p>
                     <p className="card-text ">{item.description}</p>
-                    <div className="d-flex justify-content-between align-items-center">
-                      <div className="btn-group">
-                        <button
-                          type="button"
-                          className="btn btn-sm btn-outline-secondary"
-                        >
-                          View
-                        </button>
-                        <button
-                          type="button"
-                          className="btn btn-sm btn-outline-primary"
-                        >
-                          Edit
-                        </button>
-                        <button
-                          type="button"
-                          className="btn btn-sm btn-outline-danger"
-                        >
-                          Delete
-                        </button>
-                      </div>
-                      <small className="text-body-secondary text-capitalatze fw-bold">
-                        {item.author.username}
-                      </small>
+                  </div>
+                  <div className="card-footer d-flex justify-content-between align-items-center">
+                    <div className="btn-group">
+                      <button
+                        type="button"
+                        className="btn btn-sm btn-outline-secondary"
+                      >
+                        View
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-sm btn-outline-primary"
+                      >
+                        Edit
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-sm btn-outline-danger"
+                      >
+                        Delete
+                      </button>
                     </div>
+                    <small className="text-body-secondary text-capitalize fw-bold">
+                      {item.author.username}
+                    </small>
                   </div>
                 </div>
               </div>
---
+++ src/ui/Loader.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import ContentLoader from "react-content-loader";
+
+function Loader() {
+  return (
+    <div className="row  p-0 m-0">
+      <div className="col-12 col-sm-6 col-md-4">
+        <ContentLoader viewBox="0 0 300 200" width="100%" height="auto">
+          <rect x="10" y="10" rx="4" ry="4" width="280" height="150" />
+          <rect x="10" y="170" rx="4" ry="4" width="200" height="15" />
+          <rect x="10" y="190" rx="4" ry="4" width="140" height="15" />
+        </ContentLoader>
+      </div>
+      <div className="col-12 col-sm-6 col-md-4">
+        <ContentLoader viewBox="0 0 300 200" width="100%" height="auto">
+          <rect x="10" y="10" rx="4" ry="4" width="280" height="150" />
+          <rect x="10" y="170" rx="4" ry="4" width="200" height="15" />
+          <rect x="10" y="190" rx="4" ry="4" width="140" height="15" />
+        </ContentLoader>
+      </div>
+      <div className="col-12 col-sm-6 col-md-4">
+        <ContentLoader viewBox="0 0 300 200" width="100%" height="auto">
+          <rect x="10" y="10" rx="4" ry="4" width="280" height="150" />
+          <rect x="10" y="170" rx="4" ry="4" width="200" height="15" />
+          <rect x="10" y="190" rx="4" ry="4" width="140" height="15" />
+        </ContentLoader>
+      </div>
+      <div className="col-12 col-sm-6 col-md-4">
+        <ContentLoader viewBox="0 0 300 200" width="100%" height="auto">
+          <rect x="10" y="10" rx="4" ry="4" width="280" height="150" />
+          <rect x="10" y="170" rx="4" ry="4" width="200" height="15" />
+          <rect x="10" y="190" rx="4" ry="4" width="140" height="15" />
+        </ContentLoader>
+      </div>
+      <div className="col-12 col-sm-6 col-md-4">
+        <ContentLoader viewBox="0 0 300 200" width="100%" height="auto">
+          <rect x="10" y="10" rx="4" ry="4" width="280" height="150" />
+          <rect x="10" y="170" rx="4" ry="4" width="200" height="15" />
+          <rect x="10" y="190" rx="4" ry="4" width="140" height="15" />
+        </ContentLoader>
+      </div>
+      <div className="col-12 col-sm-6 col-md-4">
+        <ContentLoader viewBox="0 0 300 200" width="100%" height="auto">
+          <rect x="10" y="10" rx="4" ry="4" width="280" height="150" />
+          <rect x="10" y="170" rx="4" ry="4" width="200" height="15" />
+          <rect x="10" y="190" rx="4" ry="4" width="140" height="15" />
+        </ContentLoader>
+      </div>
+      <div className="col-12 col-sm-6 col-md-4">
+        <ContentLoader viewBox="0 0 300 200" width="100%" height="auto">
+          <rect x="10" y="10" rx="4" ry="4" width="280" height="150" />
+          <rect x="10" y="170" rx="4" ry="4" width="200" height="15" />
+          <rect x="10" y="190" rx="4" ry="4" width="140" height="15" />
+        </ContentLoader>
+      </div>
+      <div className="col-12 col-sm-6 col-md-4">
+        <ContentLoader viewBox="0 0 300 200" width="100%" height="auto">
+          <rect x="10" y="10" rx="4" ry="4" width="280" height="150" />
+          <rect x="10" y="170" rx="4" ry="4" width="200" height="15" />
+          <rect x="10" y="190" rx="4" ry="4" width="140" height="15" />
+        </ContentLoader>
+      </div>
+      <div className="col-12 col-sm-6 col-md-4">
+        <ContentLoader viewBox="0 0 300 200" width="100%" height="auto">
+          <rect x="10" y="10" rx="4" ry="4" width="280" height="150" />
+          <rect x="10" y="170" rx="4" ry="4" width="200" height="15" />
+          <rect x="10" y="190" rx="4" ry="4" width="140" height="15" />
+        </ContentLoader>
+      </div>
+    </div>
+  );
+}
+
+export default Loader;
