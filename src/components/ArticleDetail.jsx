65e4e137e60c010f5753515bed2d182369915c61
JavohirXolmamatov <xolmamatovjavohir2002@gmail.com>
2025-03-05 11:47:50 +0500

    create article UI

--- src/components/ArticleDetail.jsx
+++ src/components/ArticleDetail.jsx
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import {
   getArticleDetailFailure,
@@ -7,12 +7,15 @@ import {
 } from "../slice/article";
 import ArticleService from "../service/articleService";
 import { useEffect } from "react";
+import moment from "moment";
+import Loader from "../ui/Loader";
 
 function ArticleDetail() {
   const { slug } = useParams();
   const dispatch = useDispatch();
+  const { articleDetail } = useSelector((state) => state.article);
 
-  const articleDetail = async () => {
+  const articlesDetail = async () => {
     dispatch(getArticleDetailStart());
     try {
       const response = await ArticleService.getArticleDetail(slug);
@@ -25,10 +28,44 @@ function ArticleDetail() {
   //ddwed
 
   useEffect(() => {
-    articleDetail();
+    articlesDetail();
   }, [slug]);
 
-  return <div>id: {slug}</div>;
+  return articleDetail === null ? (
+    <Loader />
+  ) : (
+    <div className="p-5 mb-4 rounded-3">
+      <div className="container-fluid py-5">
+        <h1 className="col-md-8 fw-bold">{articleDetail.article.title}</h1>
+        <p className="col-md-8 fs-5">{articleDetail.article.description}</p>
+        <p className="text-muted">
+          <span className="fw-bold">created at: </span>
+          {moment(articleDetail.article.createdAt).format("MMMM Do YYYY")}
+        </p>
+        <div className="card mb-3" style={{ maxWidth: "540px" }}>
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img
+                src={articleDetail.article.author.image}
+                className="img-fluid rounded-start"
+                alt={""}
+              />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title text-capitalize text-primary">
+                  {articleDetail.article.author.username}
+                </h5>
+                <p className="card-text">{articleDetail.article.author.bio}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div>{articleDetail.article.body}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ArticleDetail;
--- src/slice/article.js
+++ src/slice/article.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   isLoading: true,
   articles: [],
-  articleDetail: [],
+  articleDetail: null,
   error: null,
 };
 
