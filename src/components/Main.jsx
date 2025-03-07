2e26c2b98bd08871709fc4ca358b864644cf38e5
JavohirXolmamatov <xolmamatovjavohir2002@gmail.com>
2025-03-07 14:28:55 +0500

    Optimization

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,11 +8,11 @@ import {
   getArticleStart,
   getArticleSuccess,
 } from "../slice/article";
+import ArticleCard from "./ArticleCard";
 
 function Main() {
   const navigate = useNavigate();
   const { articles, isLoading } = useSelector((state) => state.article);
-  const { loggedIn, user } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
   const getArticle = async () => {
@@ -46,66 +46,11 @@ function Main() {
         <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
           {articles &&
             articles.map((item) => (
-              <div className="col" key={item.slug}>
-                <div className="card h-100 shadow-sm">
-                  <svg
-                    className="bd-placeholder-img card-img-top"
-                    width="100%"
-                    height="225"
-                    xmlns="http://www.w3.org/2000/svg"
-                    role="img"
-                    aria-label="Placeholder: Thumbnail"
-                    preserveAspectRatio="xMidYMid slice"
-                    focusable="false"
-                  >
-                    <title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="#55595c"></rect>
-                  </svg>
-                  <div className="card-body">
-                    <p className="card-text fw-bold m-0">{item.title}</p>
-                    <p className="card-text ">{item.description}</p>
-                  </div>
-                  <div className="card-footer d-flex justify-content-between align-items-center">
-                    <div className="btn-group">
-                      <button
-                        type="button"
-                        className="btn btn-sm btn-outline-secondary"
-                        onClick={() => {
-                          navigate(`/article/${item.slug}`);
-                        }}
-                      >
-                        View
-                      </button>
-                      {loggedIn &&
-                      user.user.username === item.author.username ? (
-                        <>
-                          <button
-                            type="button"
-                            className="btn btn-sm btn-outline-primary"
-                            onClick={() =>
-                              navigate(`edit-article/${item.slug}`)
-                            }
-                          >
-                            Edit
-                          </button>
-                          <button
-                            type="button"
-                            className="btn btn-sm btn-outline-danger"
-                            onClick={() => deleteArticle(item.slug)}
-                          >
-                            Delete
-                          </button>
-                        </>
-                      ) : (
-                        <></>
-                      )}
-                    </div>
-                    <small className="text-body-secondary text-capitalize fw-bold">
-                      {item.author.username}
-                    </small>
-                  </div>
-                </div>
-              </div>
+              <ArticleCard
+                item={item}
+                deleteArticle={deleteArticle}
+                key={item.slug}
+              />
             ))}
         </div>
       </div>
